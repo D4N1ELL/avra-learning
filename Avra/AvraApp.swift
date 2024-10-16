faec14de2b6dcaa33ca606aea38775d93c1c4553
Daniel Istrati <danielistrati767@gmail.com>
2024-10-16 23:29:12 +0300

    Initial Commit

---
+++ Avra/AvraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvraApp.swift
+//  Avra
+//
+//  Created by Daniel Istrati on 16.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AvraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
