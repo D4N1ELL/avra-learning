3b2efe41e7d94446771609ca74894e748338f999
Daniel Istrati <danielistrati767@gmail.com>
2024-10-31 21:33:23 +0200

    first commit

---
+++ Avra/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  Avra
+//
+//  Created by Daniel Istrati on 17.10.2024.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryView()
+}
---
+++ Avra/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  Avra
+//
+//  Created by Daniel Istrati on 17.10.2024.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
